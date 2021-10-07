6a0b3a7fb94707b26f92da3519f9f4f9da46722c
Vipin Choudhary <vipin.csjmk@gmail.com>
2021-10-07 13:06:47 +0100

    FirstCommit

---
+++ ECommerce.Api.Products/Db/ProductsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Api.Products.Db
+{
+    public class ProductsDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public ProductsDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
